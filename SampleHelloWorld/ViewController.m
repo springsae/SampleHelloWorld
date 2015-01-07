8d65b0bd277e20c6a8d623d6572c736c9cc99040
Saeko Fuse <saeko@SAEKO-no-MacBook-Air.local>
2015-01-07 13:41:12 +0800

    Initial Commit

---
+++ SampleHelloWorld/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  SampleHelloWorld
+//
+//  Created by Saeko Fuse on 2015/01/07.
+//  Copyright (c) 2015年 Saeko Fuse. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
